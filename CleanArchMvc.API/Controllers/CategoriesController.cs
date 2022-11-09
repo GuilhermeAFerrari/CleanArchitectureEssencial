05692e39ba1ca200d6cc02e46d9f89ad77c39db3
Guilherme Ferrari <guile.ferrari@hotmail.com>
2022-11-08 21:32:30 -0300

    feat: New controller Category API

---
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,49 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategoriesAsync();
+            if (categories is null)
+                return NotFound("Categories not found");
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id:int}", Name = "GetCategory")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            if (category is null)
+                return NotFound("Category not found");
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDTO)
+        {
+            if (categoryDTO is null)
+                return BadRequest("Invalid data");
+
+            await _categoryService.AddAsync(categoryDTO);
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDTO.Id }, categoryDTO);            
+        }
+    }
+}
