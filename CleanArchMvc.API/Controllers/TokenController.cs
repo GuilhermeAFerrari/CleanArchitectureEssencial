f1e6361d884e9cf25f0c0f8313cafb48e0191070
Guilherme Ferrari <guile.ferrari@hotmail.com>
2022-11-17 20:24:22 -0300

    feature: creted method generate token to implement jwt

--- CleanArchMvc.API/Controllers/TokenController.cs
+++ CleanArchMvc.API/Controllers/TokenController.cs
@@ -1,33 +1,79 @@
 ﻿using CleanArchMvc.API.DTOs;
 using CleanArchMvc.Domain.Account;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
-namespace CleanArchMvc.API.Controllers
+namespace CleanArchMvc.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TokenController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TokenController : ControllerBase
+    private readonly IAuthenticate _authentication;
+    private readonly IConfiguration _configuration;
+    public TokenController(IAuthenticate authentication, IConfiguration configuration)
     {
-        private readonly IAuthenticate _authentication;
+        _authentication = authentication ?? throw new ArgumentNullException(nameof(authentication));
+        _configuration = configuration;
+    }
 
-        public TokenController(IAuthenticate authentication)
+    [HttpPost("LoginUser")]
+    public async Task<ActionResult<UserToken>> Login([FromBody] LoginDTO loginDTO)
+    {
+        var result = await _authentication.AuthenticateAsync(loginDTO.Email, loginDTO.Password);
+
+        if (result)
+            return GenerateToken(loginDTO);
+
+        ModelState.AddModelError(string.Empty, "Invalid login attempt");
+        return BadRequest(ModelState);
+    }
+
+    private UserToken GenerateToken(LoginDTO loginDTO)
+    {
+        // user declarations
+        var claims = new[]
         {
-            _authentication = authentication ??
-                throw new ArgumentNullException(nameof(authentication));
-        }
+            new Claim("email", loginDTO.Email),
+            //new Claim("some value", "what you want"),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) // Jti representa Id do token
+        };
 
-        [HttpPost("LoginUser")]
-        public async Task<ActionResult<UserToken>> Login([FromBody] LoginDTO loginDTO)
+        // generate private key for assign token
+        var privateKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+
+        // generate digital signature
+        var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+        // set expiration time
+        var expiration = DateTime.UtcNow.AddMinutes(10);
+
+        // generate token
+        JwtSecurityToken token = new (
+            // issuer
+            issuer: _configuration["Jwt:Issuer"],
+
+            // audience
+            audience: _configuration["Jwt:Audience"],
+
+            // claims
+            claims: claims,
+
+            // expiration date
+            expires: expiration,
+
+            // digital signature
+            signingCredentials: credentials
+        );
+
+        return new UserToken()
         {
-            var result = await _authentication.AuthenticateAsync(loginDTO.Email, loginDTO.Password);
-
-            if (result)
-                //return GenerateToken(loginDTO);
-                return Ok($"User {loginDTO.Email} login successfully");
-
-            ModelState.AddModelError(string.Empty, "Invalid login attempt");
-            return BadRequest(ModelState);
-        }
+            Token = new JwtSecurityTokenHandler().WriteToken(token),
+            Expiration = expiration
+        };
     }
 }
