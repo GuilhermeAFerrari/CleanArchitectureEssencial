712617f35b47070574897ef7ecf6861ff11708e7
Guilherme Ferrari <guile.ferrari@hotmail.com>
2022-11-19 13:40:28 -0300

    feat: configured api to accept request with bearer token

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -1,15 +1,16 @@
 using CleanArchMvc.Infra.IoC;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddInfrastructureAPI(builder.Configuration);
 builder.Services.AddInfrastructureJWT(builder.Configuration);
+builder.Services.AddInfrastructureSwagger();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
